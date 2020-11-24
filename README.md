1e039fb864b71c952004ccf025e27c486481622d
Amy <leiamy12@gmail.com>
2020-11-24 11:13:48 -0500

    Set up routes and navbar

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # MIT SWE Attendance Tracker
+
+To run the app locally:
+1) Add database credentials to a `.env` file
+2) Run `npm run server` to start the backend
+3) Run `npm start` to star the frontend
+4) Navigate to `localhost:3000`
--- src/App.js
+++ src/App.js
@@ -1,10 +1,67 @@
-import {useEffect} from 'react';
+import {useState, useEffect} from 'react';
+import {
+    BrowserRouter as Router,
+    Link,
+    Route,
+    Switch,
+    useLocation,
+    useHistory,
+} from 'react-router-dom';
+import { Menu } from 'semantic-ui-react';
+
+import './style/app.css';
+import 'semantic-ui-css/semantic.min.css';
+
+const ACTIONS = {
+    'add': ['event', 'board-member', 'general-member'],
+    'log': ['event-attendance', 'volunteer-hours'],
+    'view': ['board-requirements', 'event-attendance'],
+}
 
 function App() {
     return (
-        <div className="App">
-            Hello World
-        </div>
+        <Router>
+            <NavBar/>
+            <Switch>
+                <Route path='/add/:type'>
+                    Add
+                </Route>
+                <Route path='/log/:type'>
+                    Log
+                </Route>
+                <Route path='/view/:type'>
+                    View
+                </Route>
+                <Route path='/'>
+                    <div className="app">
+                        Hello World
+                    </div>
+                </Route>
+            </Switch>
+        </Router>
+    );
+}
+
+function NavBar() {
+    const history = useHistory();
+    const loc = useLocation();
+    const menu = [];
+    Object.keys(ACTIONS)
+        .forEach((action) => {
+            ACTIONS[action].forEach((type) => {
+                let url = `/${action}/${type}`;
+                let label = action[0] + action.substring(1) + ' ' + type.replace('-', ' ');
+                menu.push(<Menu.Item 
+                    name={label}
+                    active={loc.pathname === url}
+                    onClick={() => history.push(url)}
+                />);
+            });
+        });
+    return (
+        <Menu text vertical className='nav'>
+            {menu}
+        </Menu>
     );
 }
 
